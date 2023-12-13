b98536be8d28a3f8f6cd305a5760d02adec994db
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2023-12-13 00:13:37 -0500

    Graficos

--- src/app/pages/charts/charts.component.ts
+++ src/app/pages/charts/charts.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild, ElementRef} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from  '@angular/common/http';
+
+import { Cancion } from '../../interfaces/cancion';
+import { SpotifySongsStatsService } from '../../providers/spotify-songs-stats.service';
+import { ProcesadorChartService } from '../../servicios/procesador-chart.service';
+import { Chart } from 'chart.js';
+import { NgChartsModule } from 'ng2-charts';
 
 @Component({
   selector: 'app-charts',
   standalone: true, 
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule, NgChartsModule],
+  providers: [SpotifySongsStatsService],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.css'
 })
 export class ChartsComponent {
+
+  @ViewChild('CaracteristicasRadar', { static: false }) CaracteristicasRadar!: ElementRef;
+  @ViewChild('Explicit', { static: false }) Explicit!: ElementRef;
+
+  constructor(private dataProvider: SpotifySongsStatsService, private chartMaker: ProcesadorChartService){}
+  ngOnInit(){
+    this.dataProvider.getResponse().subscribe((response) => {
+      let dataArray = (response as Cancion[]);
+      console.log(dataArray.length);
+      this.makeRadarChart(dataArray);
+      this.makeBarChart(dataArray);
+    });
+  }
+
+  makeRadarChart(arr:Cancion[]){
+    const ref = this.CaracteristicasRadar.nativeElement;
+    const data = this.chartMaker.getDataRadarChart(arr);
+    const options = {
+      elements: {
+        line: {
+          borderWidth: 3
+        }
+      }
+    }
+    new Chart(ref, {
+      type: "radar",
+      data: data,
+      options: options
+    });
+  }
+
+  makeBarChart(arr:Cancion[]){
+    const ref = this.Explicit.nativeElement;
+    const data = this.chartMaker.getDataBarChart(arr);
+    new Chart(ref, {
+      type: "bar",
+      data: data
+    });
+  }
 
 }
--- src/app/servicios/procesador-chart.service.ts
+++ src/app/servicios/procesador-chart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Chart } from '../interfaces/chart';
+import { Cancion } from '../interfaces/cancion';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +7,70 @@ import { Chart } from '../interfaces/chart';
 export class ProcesadorChartService {
 
   constructor() { }
+
+  private getPromedioAtributo(arr:Cancion[], atributo:keyof Cancion){
+    let sumatoria:number = 0;
+    arr.forEach((cancion:Cancion) =>{
+      const valor: number = parseFloat(cancion[atributo]);
+      sumatoria += valor;
+    });
+    return (sumatoria/arr.length);
+  }
+
+  private processDataRadar(arr: Cancion[]){
+    let arrCarac: number[] = [
+      this.getPromedioAtributo(arr, "acousticness"),
+      this.getPromedioAtributo(arr, "energy"),
+      this.getPromedioAtributo(arr, "danceability"),
+      this.getPromedioAtributo(arr, "instrumentalness")
+    ];
+    return arrCarac;
+  }
+
+  public getDataRadarChart(arr: Cancion[]){
+    const data = {
+      labels: [
+        "Acústico",
+        "Energético",
+        "Bailable",
+        "Sin instrumentación"
+      ],
+      datasets:[{
+        label:"Características de las Canciones",
+        data: this.processDataRadar(arr),
+        fill: true,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      }]
+    };
+
+    return data;
+  }
+
+  public getDataBarChart(arr: Cancion[]){
+    const data = {
+      labels:[
+        "Explícitas",
+        "No Explícitas"
+      ],
+      datasets:[{
+        label: "Explícitas vs No Explícitas",
+        data:[
+          arr.filter(cancion => cancion.is_explicit === "True").length,
+          arr.filter(cancion => cancion.is_explicit !== "True").length,
+        ],
+        fill:true,
+        backgroundColor:[
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+        ],
+        borderWidth: 1
+      }]
+    }
+    return data
+  }
+
 }
